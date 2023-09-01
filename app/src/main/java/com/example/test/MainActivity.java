87c30b7ea477712c79f13563dede3bd043d91ee0
bayukrisna <bayukrisna14658@gmail.com>
2023-09-01 13:47:19 +0700

    baru

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -11,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
